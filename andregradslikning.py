b30ff6b8c3a4b717d6f93be6b19d6de058260e79
Jon Bjarne <jbb_598@hotmail.com>
2024-10-04 12:32:20 +0200

    test

--- andregradslikning.py
+++ andregradslikning.py
@@ -1,15 +1,15 @@
 # Finner løsninger til alle andregradslikninger på formen ax^2+bx+c=0
-from math import sqrt
+import math
 
 def losninger(a, b, c):
   diskriminant = b**2 - 4*a*c
   if diskriminant < 0:
     return "Likningen har ingen løsning!"
   
-  losning1 = round((-b + sqrt(diskriminant))/(2*a), 2)
-  losning2 = round((-b - sqrt(diskriminant))/(2*a), 2)
+  losning1 = round((-b + math.sqrt(diskriminant))/(2*a), 2)
+  losning2 = round((-b - math.sqrt(diskriminant))/(2*a), 2)
   
   if diskriminant == 0:
-    return losning1 #Et flyttall avrundet til to desimaler.
+    return losning1 # Et flyttall avrundet til to desimaler.
   else:
     return (losning1, losning2) #En tuppel med to flyttall som begge er avrundet til to desimaler.
